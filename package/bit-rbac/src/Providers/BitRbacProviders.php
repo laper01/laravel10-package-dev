f177581b2b5b75e52051fa6ff4c7ecf81a9de117
laper01 <yusufyahya0107@gmail.com>
2023-03-28 11:52:44 +0800

    install package and command line fix

--- package/bit-rbac/src/Providers/BitRbacProviders.php
+++ package/bit-rbac/src/Providers/BitRbacProviders.php
@@ -4,16 +4,36 @@ namespace Danova\BitRbac\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Danova\BitRbac\Console\InstallBitRBAC;
+
 class BitRbacProviders extends ServiceProvider
 {
+
     /**
      * Bootstrap services.
      *
      * @return void
      */
+
     public function boot()
     {
         //
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallBitRBAC::class
+            ]);
+        }
+
+        // $this->commands([
+        //     Console\InstallBitRBAC::class,
+        // ]);
+    }
+
+    public function provides()
+    {
+        return [InstallBitRBAC::class];
     }
 }
