95681b540787a3d16109c07f7a1e5acce4aeb957
laper01 <yusufyahya0107@gmail.com>
2023-04-03 15:41:51 +0800

    add menu model and migration

---
+++ package/bit-rbac/src/database/migrations/2023_04_01_074223_create_menus_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->integer("depth")->nullable();
+            $table->integer("position")->require();
+            $table->string("icon_class",30)->nullable();
+            $table->enum('type',["LINK","MODULE","GROUP"])->nullable();
+            $table->string("name",30)->require();
+            $table->string("original_name",30)->nullable();
+            $table->integer('parent_menu_id')->nullable();
+            $table->boolean('is_active')->require();
+            $table->integer('module_id')->nullable();
+            $table->string('url')->nullable();
+            $table->boolean('open_in_new_tab')->require();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
