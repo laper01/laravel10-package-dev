77f876b0d71eb59d3bb8323978bcedb4d1b33b97
laper01 <yusufyahya0107@gmail.com>
2023-04-06 15:29:47 +0800

    add routes property

--- package/bit-rbac/src/Console/InstallMenu.php
+++ package/bit-rbac/src/Console/InstallMenu.php
@@ -4,8 +4,8 @@ namespace Danova\BitRbac\Console;
 
 use Illuminate\Console\Command;
 use File;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 
 class InstallMenu extends Command
@@ -17,7 +17,8 @@ class InstallMenu extends Command
      */
 
     protected $file;
-    protected $foleder;
+    protected $folder;
+    protected $routes;
 
     protected $signature = 'bitrbac-api:install-menu';
 
@@ -28,6 +29,23 @@ class InstallMenu extends Command
      */
     protected $description = 'add menu at rbac system';
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->routes = collect(Route::getRoutes())->map(function ($route) {
+            return [
+                'method' => implode('|', $route->methods()),
+                'uri' => $route->uri(),
+                'name' => $route->getName(),
+                'action' => ltrim($route->getActionName(), '\\'),
+                'middleware' => collect($route->middleware())
+                    ->filter(function (string $string) {
+                        return Str::startsWith($string, 'rbac');
+                    })->first()
+            ];
+        });
+    }
+
     /**
      * Execute the console command.
      */
@@ -37,14 +55,14 @@ class InstallMenu extends Command
         // ============================
         // $routes = Route::getRoutes();
         // dd($routes);
-        $this->listFile();
+        dd($this->routes);
     }
 
 
     public function propOfFile($allFiles): array
     {
-        $data= [];
-        foreach ($allFiles as $key => $value){
+        $data = [];
+        foreach ($allFiles as $key => $value) {
             array_push($data, $value->getFilename());
         }
 
